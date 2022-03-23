d9ef07ba4abb6850df1024af0e88ad59f90fe618
ambika13kumari <86556750+ambika13kumari@users.noreply.github.com>
2022-03-23 14:25:00 +0530

    Pushed all files

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const router = express()
+const User = require("../models/login.model")
+router.post("/", async (req, res) => {
+
+   const  user = await User.create(req.body)
+    return res.status(201).send(user)
+    
+})
+
+router.get("/", async (req, res) => {
+    const user = await User.find()
+    return res.status(201).send(user)
+})
+
+router.delete("/", async (req, res) => {
+    const user = await User.findByIdAndDelete()
+})
+
+module.exports = router
